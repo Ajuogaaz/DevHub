040f4c28eb0c6826f62b7f6fa311b8716e66105e
Linus Brian Okoth <okoth@fb.com>
2020-07-12 21:27:39 -0500

    Moved main activity to activities folder

--- app/src/main/java/com/example/devhub/Activities/MainActivity.java
+++ app/src/main/java/com/example/devhub/Activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.devhub;
+package com.example.devhub.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.devhub.R;
 
 public class MainActivity extends AppCompatActivity {
 
