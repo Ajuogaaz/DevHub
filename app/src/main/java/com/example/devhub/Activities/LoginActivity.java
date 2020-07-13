43c3452578d0231439175c30e606633f4e207036
Linus Brian Okoth <okoth@fb.com>
2020-07-13 10:58:52 -0500

    Adding Binding

--- app/src/main/java/com/example/devhub/Activities/LoginActivity.java
+++ app/src/main/java/com/example/devhub/Activities/LoginActivity.java
@@ -2,15 +2,96 @@ package com.example.devhub.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.devhub.R;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    public static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+
+        final ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
+
+        // layout of activity is stored in a special property called root
+        View view = binding.getRoot();
+        setContentView(view);
+
+        if(ParseUser.getCurrentUser() != null) {
+            goToMainActivity();
+        }
+
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "OnClick login button");
+                String username = binding.etUsername.getText().toString();
+                String password = binding.etPassword.getText().toString();
+                Toast.makeText(LoginActivity.this, "login in", Toast.LENGTH_SHORT).show();
+                loginUser(username, password);
+            }
+        });
+        binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "Onclick dignIn");
+
+                ParseUser user = new ParseUser();
+
+                user.setUsername(binding.etUsername.getText().toString());
+                user.setPassword(binding.etPassword.getText().toString());
+
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            goToMainActivity();
+                        }else{
+                            Toast.makeText(LoginActivity.this, "User Already exists", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
     }
+
+    private void loginUser(String username, String password) {
+
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Issue with login" + e);
+                    return;
+                }
+
+                goToMainActivity();
+
+            }
+        });
+
+    }
+
+    private void goToMainActivity() {
+
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
