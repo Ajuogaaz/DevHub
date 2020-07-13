d9f97e2ef84e5cb18aaf2f3059d5cabb200d58f1
Linus Brian Okoth <okoth@fb.com>
2020-07-13 19:02:43 -0400

    Imported retrofit http header and Post

--- app/src/main/java/com/example/devhub/network/ApiClient.java
+++ app/src/main/java/com/example/devhub/network/ApiClient.java
@@ -1,4 +1,10 @@
 package com.example.devhub.network;
 
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
 public interface ApiClient {
+    @Headers("Accept: application/json")
+    @POST("login/oauth/access_token")
+
 }
