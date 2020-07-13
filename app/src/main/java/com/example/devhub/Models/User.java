e39bbab656d1470d50c66ca331e9309be64454d1
Linus Brian Okoth <okoth@fb.com>
2020-07-13 09:22:43 -0500

    Updsated user

--- app/src/main/java/com/example/devhub/Models/User.java
+++ app/src/main/java/com/example/devhub/Models/User.java
@@ -2,14 +2,30 @@ package com.example.devhub.Models;
 
 
 import com.parse.ParseClassName;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
 
 @ParseClassName("User")
 public class User extends ParseUser {
 
-    public static final String KEY_NAME = "name";
-    public static final String KEY_ = "postImage";
-    public static final String KEY_USER = "postingUser";
-    public static final String KEY_CREATED_AT = "createdAt";
+    public static final String KEY_NAME = "username";
+    public static final String KEY_AUTH = "authData";
+    public static final String KEY_PROFILE_PIC = "profilePic";
+
+
+
+    public ParseFile getImage(){
+        return getParseFile(KEY_PROFILE_PIC);
+    }
+    public void setImage(ParseFile parseFile){
+        put (KEY_PROFILE_PIC, parseFile);
+    }
+
+    public String getAuth(){
+        return getString(KEY_AUTH);
+    }
+    public void setAuth(String auth){
+        put (KEY_AUTH, auth);
+    }
 
 }
