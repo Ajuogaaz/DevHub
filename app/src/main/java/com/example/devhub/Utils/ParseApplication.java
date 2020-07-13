f829d2c375f379220e4ad8a184249481004b3bd6
Linus Brian Okoth <okoth@fb.com>
2020-07-12 22:06:02 -0500

    Added parse appliaction configurations

--- app/src/main/java/com/example/devhub/Utils/ParseApplication.java
+++ app/src/main/java/com/example/devhub/Utils/ParseApplication.java
@@ -20,9 +20,9 @@ public class ParseApplication extends Application {
         // clientKey is not needed unless explicitly configured
         // any network interceptors must be added with the Configuration Builder given this syntax
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("linusgram") // should correspond to APP_ID env variable
+                .applicationId("devh-ub") // should correspond to APP_ID env variable
                 .clientKey("key")  // set explicitly unless clientKey is explicitly configured on Parse server
-                .server("https://linusgram.herokuapp.com/parse/").build());
+                .server("https://devh-ub.herokuapp.com/parse/").build());
 
     }
 }
