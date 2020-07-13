b04595fcd87efea103ba338599794892146cad58
Linus Brian Okoth <okoth@fb.com>
2020-07-13 09:46:50 -0500

    Created Comments.java

--- app/src/main/java/com/example/devhub/Models/Comments.java
+++ app/src/main/java/com/example/devhub/Models/Comments.java
@@ -4,4 +4,14 @@ import com.parse.ParseObject;
 
 
 public class Comments extends ParseObject {
+
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_IMAGE = "postImage";
+    public static final String KEY_USER = "postingUser";
+    public static final String KEY_CREATED_AT = "createdAt";
+    public static final String KEY_POST = "relatedPost";
+
+
+
+
 }
