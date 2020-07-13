701a4b00516f2c1459e3c6d11b14ec7a4d441dd7
Linus Brian Okoth <okoth@fb.com>
2020-07-13 19:08:55 -0400

    Added the get Retrofit method

--- app/src/main/java/com/example/devhub/network/ApiService.java
+++ app/src/main/java/com/example/devhub/network/ApiService.java
@@ -1,7 +1,9 @@
 package com.example.devhub.network;
 
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiService {
 
@@ -10,5 +12,25 @@ public class ApiService {
     private static Retrofit sRetrofit = null;
     private static Retrofit userRepos = null;
     private static OkHttpClient sOkHttpClient;
+
+    private static Retrofit getRetrofit() {
+        if (sRetrofit == null) {
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+            sOkHttpClient = logging(logging);
+
+            sRetrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(sOkHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return sRetrofit;
+    }
+
+    private static OkHttpClient logging(HttpLoggingInterceptor logging) {
+    }
+
 
 }
