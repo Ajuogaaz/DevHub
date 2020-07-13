f7b60ccec410c9a3d0a9c5b7fb22eb7bedea59ec
Linus Brian Okoth <okoth@fb.com>
2020-07-13 16:13:45 -0500

    Added validate activity_validate.xml

---
+++ app/src/main/java/com/example/devhub/Activities/ValidateActivity.java
@@ -0,0 +1,14 @@
+package com.example.devhub.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ValidateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validate);
+    }
+}
