424a6192a984e31cfd04b528b0ffcfc90f326167
Linus Brian Okoth <okoth@fb.com>
2020-07-13 16:58:02 -0500

    Added Access Token class

---
+++ app/src/main/java/com/example/devhub/Utils/AccessToken.java
@@ -0,0 +1,50 @@
+package com.example.devhub.Utils;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
+public class AccessToken implements Parcelable {
+
+    @SerializedName("access_token")
+    private String accessToken;
+    @SerializedName("token_type")
+    private String tokenType;
+
+    protected AccessToken(Parcel in) {
+        accessToken = in.readString();
+        tokenType = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(accessToken);
+        dest.writeString(tokenType);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<AccessToken> CREATOR = new Creator<AccessToken>() {
+        @Override
+        public AccessToken createFromParcel(Parcel in) {
+            return new AccessToken(in);
+        }
+
+        @Override
+        public AccessToken[] newArray(int size) {
+            return new AccessToken[size];
+        }
+    };
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+}
