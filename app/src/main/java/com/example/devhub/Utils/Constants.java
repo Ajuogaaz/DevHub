664b64462755d861d1cb4334b9a9745c11a2f03b
Linus Brian Okoth <okoth@fb.com>
2020-07-13 16:42:06 -0500

    Created Constants

--- app/src/main/java/com/example/devhub/Utils/Constants.java
+++ app/src/main/java/com/example/devhub/Utils/Constants.java
@@ -1,4 +1,13 @@
 package com.example.devhub.Utils;
 
 public class Constants {
+    //This is the class where I store all the constants that I will be using fpr network calls
+
+    public static final String CLIENT_ID = "4fef7790e8e9e860608f";
+    public static final String CLIENT_SECRET = "c2794afe66be45628301867e6b6aacb46d3dd742";
+    public static final String REDIRECT_URI = "edtrum://callback";
+    public static final String GITHUB_URI = "https://github.com/login/oauth/authorize";
+    public static final String AUTH_URL = "https://api.github.com/";
+
+
 }
