0c694779e49ee749acb6a73854644f35bbd17f47
Linus Brian Okoth <okoth@fb.com>
2020-07-13 09:55:47 -0500

    Posted comments

--- app/src/main/java/com/example/devhub/Models/Comments.java
+++ app/src/main/java/com/example/devhub/Models/Comments.java
@@ -1,15 +1,37 @@
 package com.example.devhub.Models;
 
+import com.parse.ParseFile;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 
 public class Comments extends ParseObject {
 
     public static final String KEY_DESCRIPTION = "description";
-    public static final String KEY_IMAGE = "postImage";
     public static final String KEY_USER = "postingUser";
     public static final String KEY_CREATED_AT = "createdAt";
     public static final String KEY_POST = "relatedPost";
+
+    public String getDescription(){
+        return getString(KEY_DESCRIPTION);
+    }
+    public void setDescription(String description){
+        put (KEY_DESCRIPTION, description);
+    }
+
+    public Post getPost(){
+        return (Post)getParseObject(KEY_POST);
+    }
+    public void setPost(Post post){
+        put (KEY_POST, post);
+    }
+
+    public ParseUser getUser(){
+        return getParseUser(KEY_USER);
+    }
+    public void setUser(ParseUser user){
+        put (KEY_USER, user);
+    }
 
 
 
