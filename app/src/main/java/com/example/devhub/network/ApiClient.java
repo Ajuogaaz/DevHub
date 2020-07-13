6a48f9719e28a1ea085dd11f190addbadaa38409
Linus Brian Okoth <okoth@fb.com>
2020-07-13 19:04:00 -0400

    Added get access token methods

--- app/src/main/java/com/example/devhub/network/ApiClient.java
+++ app/src/main/java/com/example/devhub/network/ApiClient.java
@@ -1,10 +1,21 @@
 package com.example.devhub.network;
 
+import com.example.devhub.Utils.AccessToken;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface ApiClient {
     @Headers("Accept: application/json")
     @POST("login/oauth/access_token")
+    @FormUrlEncoded
+    Call<AccessToken> getAccessToken(
+            @Field("client_id") String clientId,
+            @Field("client_secret") String clientSecret,
+            @Field("code") String accessToken
+    );
 
 }
