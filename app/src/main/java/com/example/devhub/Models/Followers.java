bd77f7cba45d9085c675ea9d08fa8f27fedb1410
Linus Brian Okoth <okoth@fb.com>
2020-07-13 10:27:05 -0500

    Added method to following user

--- app/src/main/java/com/example/devhub/Models/Followers.java
+++ app/src/main/java/com/example/devhub/Models/Followers.java
@@ -2,6 +2,10 @@ package com.example.devhub.Models;
 
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @ParseClassName("Followers")
 public class Followers extends ParseObject {
@@ -11,6 +15,39 @@ public class Followers extends ParseObject {
     public static final String KEY_CREATED_AT = "createdAt";
 
 
+    public User getSubjectUser(){
+        return (User)getParseUser(KEY_USER);
+    }
+    public void setSubjectUser(User user){
+        put (KEY_USER, user);
+    }
+
+    public User getFollowingUser(){
+        return (User)getParseUser(KEY_USER);
+    }
+    public void setFollowingUser(User user){
+        put (KEY_USER, user);
+    }
+
+
+    public ParseUser getUser(){
+        return getParseUser(KEY_USER);
+    }
+    public void setUser(ParseUser user){
+        put (KEY_USER, user);
+    }
+
+    public String getTime(){
+
+        Date date = getCreatedAt();
+
+        SimpleDateFormat format = new SimpleDateFormat("E MM dd yyyy hh:mm");
+
+        String currDate = format.format(date);
+
+        return currDate;
+
+    }
 
 
 
