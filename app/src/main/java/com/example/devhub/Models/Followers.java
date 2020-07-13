74861b60ef97468fe9bafd45f6c551770f384341
Linus Brian Okoth <okoth@fb.com>
2020-07-13 10:28:53 -0500

    Finished up following

--- app/src/main/java/com/example/devhub/Models/Followers.java
+++ app/src/main/java/com/example/devhub/Models/Followers.java
@@ -2,7 +2,6 @@ package com.example.devhub.Models;
 
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
-import com.parse.ParseUser;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
