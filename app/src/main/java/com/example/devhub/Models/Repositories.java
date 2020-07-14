0edbe992c3775b2628c607cd9d381014e9bcfe23
Linus Brian Okoth <okoth@fb.com>
2020-07-13 20:19:34 -0400

    Added parceable implements

--- app/src/main/java/com/example/devhub/Models/Repositories.java
+++ app/src/main/java/com/example/devhub/Models/Repositories.java
@@ -1,4 +1,44 @@
 package com.example.devhub.Models;
 
-public class Repositories {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Repositories implements Parcelable {
+
+    @SerializedName("name")
+    private String name;
+    @SerializedName("full_name")
+    private String fullName;
+
+    protected Repositories(Parcel parcel) {
+        name = parcel.readString();
+        fullName = parcel.readString();
+    }
+
+    public static final Creator<Repositories> CREATOR = new Creator<Repositories>() {
+        @Override
+        public Repositories createFromParcel(Parcel in) {
+            return new Repositories(in);
+        }
+
+        @Override
+        public Repositories[] newArray(int size) {
+            return new Repositories[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+
+        parcel.writeString(name);
+        parcel.writeString(fullName);
+
+    }
 }
