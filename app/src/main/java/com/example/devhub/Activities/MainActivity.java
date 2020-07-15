f5991e9d55529e7f3729066ed711609ecdc64538
Linus Brian Okoth <okoth@fb.com>
2020-07-14 22:24:34 -0400

    Added Binding

--- app/src/main/java/com/example/devhub/Activities/MainActivity.java
+++ app/src/main/java/com/example/devhub/Activities/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.devhub.Activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 import com.example.devhub.R;
+import com.example.devhub.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
 
 
 
