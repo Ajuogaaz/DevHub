490eac3975c725b7281f686a4ccc677ab0a6c76b
Linus Brian Okoth <okoth@fb.com>
2020-07-15 10:31:16 -0400

    Added the repo item

--- app/src/main/java/com/example/devhub/Adapters/RepositoryAdapter.java
+++ app/src/main/java/com/example/devhub/Adapters/RepositoryAdapter.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.devhub.Models.Repositories;
@@ -52,36 +53,40 @@ public class RepositoryAdapter extends RecyclerView.Adapter<RepositoryAdapter.Vi
 
     @Override
     public int getItemCount() {
-        return mUserRepos != null ? mUserRepos.size() : 0;
+        return  repos.size();
     }
 
-    public void setData(List<UserRepo> userRepos){
+    /*public void setData(List<UserRepo> userRepos){
         mUserRepos = userRepos;
         notifyDataSetChanged();
-    }
+    }*/
 
     class ViewHolder extends RecyclerView.ViewHolder {
 
-        ItemInteraction mItemInteraction;
         TextView repoName, repoFullName;
+        CardView repoItem;
 
         ViewHolder(@NonNull View itemView, ItemInteraction itemInteraction) {
             super(itemView);
-            mItemInteraction = itemInteraction;
+
             repoName = itemView.findViewById(R.id.repo_name);
             repoFullName = itemView.findViewById(R.id.repo_full_name);
-            itemView.setOnClickListener(this);
+            repoItem = itemView.findViewById(R.id.repoItem);
+
         }
 
-        public void bind(UserRepo userRepo) {
-            repoFullName.setText(userRepo.getFullName());
-            repoName.setText(userRepo.getName());
+        public void bind(Repositories repo) {
+
+
+            repoFullName.setText(repo.getFullName());
+            repoName.setText(repo.getName());
+
+
+
+
+
         }
 
-        @Override
-        public void onClick(View v) {
-            mItemInteraction.repoItemClick(mUserRepos.get(getAdapterPosition()));
-        }
     }
 
     public interface ItemInteraction {
