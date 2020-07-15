30b183291633e30d63bd5d75926031491500547c
Linus Brian Okoth <okoth@fb.com>
2020-07-14 22:36:56 -0400

    Registered Chat and Notifications Fragment

--- app/src/main/java/com/example/devhub/Activities/MainActivity.java
+++ app/src/main/java/com/example/devhub/Activities/MainActivity.java
@@ -1,18 +1,22 @@
 package com.example.devhub.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.devhub.Fragments.RepositoryFragment;
 import com.example.devhub.Fragments.SearchFragment;
 import com.example.devhub.Fragments.TimelineFragment;
 import com.example.devhub.R;
 import com.example.devhub.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,6 +43,50 @@ public class MainActivity extends AppCompatActivity {
         // layout of activity is stored in a special property called root
         final View view = binding.getRoot();
         setContentView(view);
+
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_chat:
+                        //binding.toolbar.setVisibility(View.VISIBLE);
+                        Toast.makeText(MainActivity.this, "Chat",
+                                Toast.LENGTH_SHORT).show();
+                        fragment = ChatFragment;
+                        break;
+
+                    case R.id.action_notifications:
+                        Toast.makeText(MainActivity.this, "notifications",
+                                Toast.LENGTH_SHORT).show();
+                        binding.toolbar.setVisibility(View.GONE);
+                        fragment = NotificationFragment;
+                        break;
+
+                    case R.id.action_compose:
+                        Toast.makeText(MainActivity.this, "Compose",
+                                Toast.LENGTH_SHORT).show();
+                        binding.toolbar.setVisibility(View.GONE);
+                        fragment = composeFragment;
+                        break;
+                    case R.id.action_profile:
+                    default:
+                        Toast.makeText(MainActivity.this, "Profile",
+                                Toast.LENGTH_SHORT).show();
+                        binding.toolbar.setVisibility(View.GONE);
+                        fragment = profileFragment;
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        binding.bottomNavigation.setSelectedItemId(R.id.action_home);
+
+
+
+
 
 
 
