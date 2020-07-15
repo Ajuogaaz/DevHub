8bb6eca9a3db846d4226896a1b1cc69af31024c2
Linus Brian Okoth <okoth@fb.com>
2020-07-14 21:06:01 -0400

    Added a new notifications fragment

---
+++ app/src/main/java/com/example/devhub/Fragments/NotificationsFragment.java
@@ -0,0 +1,21 @@
+package com.example.devhub.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.devhub.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NotificationsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NotificationsFragment extends Fragment {
+
+
+}
