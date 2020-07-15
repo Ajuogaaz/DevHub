28d97ea8740bf3efd2a24b76b15828c23c84632f
Linus Brian Okoth <okoth@fb.com>
2020-07-14 22:30:11 -0400

    Added the server innitialization

--- app/src/main/java/com/example/devhub/Activities/MainActivity.java
+++ app/src/main/java/com/example/devhub/Activities/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.devhub.Activities;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
+import com.example.devhub.Fragments.RepositoryFragment;
+import com.example.devhub.Fragments.SearchFragment;
 import com.example.devhub.R;
 import com.example.devhub.databinding.ActivityMainBinding;
 
@@ -22,6 +25,12 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
         final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        //define your fragments here
+        final Fragment ChatFragment = new ChatFragment();
+        final Fragment NotificationFragment = new NotificationFragment();
+        final Fragment RepositoryFragment = new RepositoryFragment();
+        final Fragment Searchfragment = new SearchFragment();
 
 
 
