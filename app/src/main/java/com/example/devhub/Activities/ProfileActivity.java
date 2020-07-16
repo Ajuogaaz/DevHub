8f2e766ce7bf6dd6880fa2d7c0921a8efc7ab7f2
Linus Brian Okoth <okoth@fb.com>
2020-07-16 01:09:15 -0400

    Added Binding

--- app/src/main/java/com/example/devhub/Activities/ProfileActivity.java
+++ app/src/main/java/com/example/devhub/Activities/ProfileActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 import com.example.devhub.Adapters.ProfileAdapter;
 import com.example.devhub.Models.Post;
 import com.example.devhub.R;
+
 import com.example.devhub.databinding.ActivityProfileBinding;
 import com.parse.FindCallback;
 import com.parse.ParseException;
