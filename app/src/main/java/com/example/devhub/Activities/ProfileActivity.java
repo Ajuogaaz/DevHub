b47c59565ac43c79ea4102a6046b4b85a5dc4377
Linus Brian Okoth <okoth@fb.com>
2020-07-15 22:59:22 -0400

    Added the profile Activity

---
+++ app/src/main/java/com/example/devhub/Activities/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.devhub.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.devhub.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
