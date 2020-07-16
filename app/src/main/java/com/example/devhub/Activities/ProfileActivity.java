05581ede7d85effb37f57b29903171675a93309f
Linus Brian Okoth <okoth@fb.com>
2020-07-16 00:03:10 -0400

    Added posts

--- app/src/main/java/com/example/devhub/Activities/ProfileActivity.java
+++ app/src/main/java/com/example/devhub/Activities/ProfileActivity.java
@@ -3,14 +3,39 @@ package com.example.devhub.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.example.devhub.Models.Post;
 import com.example.devhub.R;
+import com.example.devhub.databinding.ActivityProfileBinding;
+
+import java.util.List;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    ActivityProfileBinding binding;
+
+    List<Post> posts
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        binding = new ActivityProfileBinding();
+
+        binding.btnEditProfile.setOnClickListener(view -> {
+
+            Toast.makeText(this, "EditProfile", Toast.LENGTH_SHORT).show();
+
+        });
+
+        bin
+
+
+
+
+
     }
 }
