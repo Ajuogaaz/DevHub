1d6df28da4b58491c21b047f6b15836457182588
Linus Brian Okoth <okoth@fb.com>
2020-07-17 08:56:24 -0400

    Finished configuration of binding

--- app/src/main/java/com/example/devhub/Activities/SettingsActivity.java
+++ app/src/main/java/com/example/devhub/Activities/SettingsActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.devhub.R;
@@ -18,9 +19,11 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
 
         binding = ActivitySettingsBinding.inflate(getLayoutInflater());
+
+        View view = binding.getRoot();
+        setContentView(view);
 
         binding.btnLogout.setOnClickListener(view -> {
             Toast.makeText(this, "Logging Out", Toast.LENGTH_SHORT).show();
