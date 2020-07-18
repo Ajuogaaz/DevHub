6eba7c61a54d5f613e7521f271072bed39976af1
Linus Brian Okoth <okoth@fb.com>
2020-07-17 23:47:11 -0400

    Updated camera functionality and Maniofest

--- app/src/main/java/com/example/devhub/Activities/ProfileActivity.java
+++ app/src/main/java/com/example/devhub/Activities/ProfileActivity.java
@@ -1,10 +1,17 @@
 package com.example.devhub.Activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -17,8 +24,11 @@ import com.example.devhub.R;
 import com.example.devhub.databinding.ActivityProfileBinding;
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,6 +39,9 @@ public class ProfileActivity extends AppCompatActivity {
     private static final String TAG = "PROFILEACTIVITY";
     List<Post> posts;
     ProfileAdapter profileAdapter;
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    File photoFile;
+    public String photoFileName = "photo.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,6 +96,10 @@ public class ProfileActivity extends AppCompatActivity {
 
         });
 
+        binding.ivProfileImage.setOnClickListener(view3 -> {
+            launchCamera();
+        });
+
         binding.rvPost.setAdapter(profileAdapter);
 
         //set the layout manager on the recycler view
@@ -93,6 +110,64 @@ public class ProfileActivity extends AppCompatActivity {
         queryposts(0);
 
     }
+
+    private void launchCamera() {
+        //Create an intent to take pictures and return control to the app
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //Create a file reference to access to future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        //wrap file object into a content provider
+        Uri fileProvider = FileProvider.getUriForFile(this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+
+    }
+
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.i(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == -1) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ParseUser currentUser = ParseUser.getCurrentUser();
+
+                currentUser.put("ProfilePic", new ParseFile(photoFile));
+                currentUser.saveInBackground(e -> {
+                    if(e==null){
+                        Log.i(TAG, "done: Saved Success");
+                    }
+                });
+
+            } else { // Result was a failure
+                Toast.makeText(ProfileActivity.this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 
     private void queryposts(final int page) {
 
