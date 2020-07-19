ab4cd15860719318f4a53194a5d775053a7593d9
Linus Brian Okoth <okoth@fb.com>
2020-07-18 21:32:28 -0400

    Modified the manifest

--- app/src/main/java/com/example/devhub/Activities/MainActivity.java
+++ app/src/main/java/com/example/devhub/Activities/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
         final FragmentManager fragmentManager = getSupportFragmentManager();
