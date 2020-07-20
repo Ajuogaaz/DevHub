73bd1fa893b6db4d65a6115a3370229c973bed38
Linus Brian Okoth <okoth@fb.com>
2020-07-20 12:39:52 -0400

    Modified the main activity

--- app/src/main/java/com/example/devhub/Activities/MainActivity.java
+++ app/src/main/java/com/example/devhub/Activities/MainActivity.java
@@ -56,10 +56,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Window w = getWindow();
-            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        }
+       
 
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
