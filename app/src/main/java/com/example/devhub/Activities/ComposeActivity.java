4b35a71d6612158b4168bd0af205c906a8bdd6c0
Linus Brian Okoth <okoth@fb.com>
2020-07-20 17:12:55 -0400

    Added all the components

--- app/src/main/java/com/example/devhub/Activities/ComposeActivity.java
+++ app/src/main/java/com/example/devhub/Activities/ComposeActivity.java
@@ -12,5 +12,7 @@ public class ComposeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
+
+
     }
 }
--- app/src/main/java/com/example/devhub/Activities/MainActivity.java
+++ app/src/main/java/com/example/devhub/Activities/MainActivity.java
@@ -56,8 +56,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       
-
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
