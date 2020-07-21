5e75b239dff6dcb1546359417d80d5c8789931f8
Linus Brian Okoth <okoth@fb.com>
2020-07-21 03:14:18 -0400

    Fixed a bug

--- app/src/main/java/com/example/devhub/Models/Post.java
+++ app/src/main/java/com/example/devhub/Models/Post.java
@@ -20,7 +20,7 @@ public class Post extends ParseObject {
     public static final String KEY_IMAGE = "postImage";
     public static final String KEY_USER = "postingUser";
     public static final String KEY_CREATED_AT = "createdAt";
-    public static final String KEY_TOPIC = "Topic";
+    public static final String KEY_TOPIC = "postTitle";
 
 
 
