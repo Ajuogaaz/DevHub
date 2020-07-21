45286e4801e31c0a96116a40d2b22611dd32f8fd
Linus Brian Okoth <okoth@fb.com>
2020-07-20 22:52:49 -0400

    Added binding

--- app/src/main/java/com/example/devhub/Activities/ComposeActivity.java
+++ app/src/main/java/com/example/devhub/Activities/ComposeActivity.java
@@ -3,15 +3,26 @@ package com.example.devhub.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.devhub.R;
+import com.example.devhub.databinding.ActivityComposeBinding;
+import com.example.devhub.databinding.ActivityProfileBinding;
+
+import org.jetbrains.annotations.NotNull;
 
 public class ComposeActivity extends AppCompatActivity {
+
+    ActivityProfileBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_compose);
+
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
+
+        final View view = binding.getRoot();
+        setContentView(view);
 
 
     }
