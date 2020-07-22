7e1757e670c881e6c33cd64e8da3c16927421604
Linus Brian Okoth <okoth@fb.com>
2020-07-21 20:13:43 -0400

    Fixed Bug and created the array List

--- app/src/main/java/com/example/devhub/Activities/DetailsActivity.java
+++ app/src/main/java/com/example/devhub/Activities/DetailsActivity.java
@@ -3,18 +3,34 @@ package com.example.devhub.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.devhub.Models.Post;
 import com.example.devhub.R;
 import com.example.devhub.databinding.ActivityDetailsBinding;
 import com.example.devhub.databinding.ActivityProfileBinding;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DetailsActivity extends AppCompatActivity {
 
     ActivityDetailsBinding binding;
+    List<Post> posts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ParseUser.getCurrentUser().fetchInBackground();
+
+        posts = new ArrayList<>();
+
+        binding = ActivityDetailsBinding.inflate(getLayoutInflater());
+
+        final View view = binding.getRoot();
+        setContentView(view);
 
 
 
