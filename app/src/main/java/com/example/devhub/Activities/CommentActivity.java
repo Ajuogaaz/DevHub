eb36316e0f991871b6e48156a9a4e1d8ca88c2a1
Linus Brian Okoth <okoth@fb.com>
2020-07-22 02:09:30 -0400

    Added Post import

--- app/src/main/java/com/example/devhub/Activities/CommentActivity.java
+++ app/src/main/java/com/example/devhub/Activities/CommentActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.devhub.Models.Post;
 import com.example.devhub.R;
 
 public class CommentActivity extends AppCompatActivity {
 
+    Post SubjectPost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
