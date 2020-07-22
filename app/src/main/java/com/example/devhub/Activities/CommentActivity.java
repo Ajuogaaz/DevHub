c9092703a0d274014fb3927c6e66a79d134cdf51
Linus Brian Okoth <okoth@fb.com>
2020-07-22 02:19:52 -0400

    Added Profile Image

--- app/src/main/java/com/example/devhub/Activities/CommentActivity.java
+++ app/src/main/java/com/example/devhub/Activities/CommentActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.bumptech.glide.Glide;
 import com.example.devhub.Models.Post;
 import com.example.devhub.R;
 import com.example.devhub.databinding.ActivityCommentBinding;
@@ -29,20 +30,51 @@ public class CommentActivity extends AppCompatActivity {
         ParseUser.getCurrentUser().fetchInBackground();
 
         binding = ActivityCommentBinding.inflate(getLayoutInflater());
-
-        binding.Cancel.setOnClickListener(view -> {
-
-            Intent intent = new Intent(CommentActivity.this, DetailsActivity.class);
-            intent.putExtra("post", SubjectPost);
-            startActivity(intent);
-            finish();
-        });
-
-        binding.
-
-
         final View view = binding.getRoot();
         setContentView(view);
+
+        binding.Cancel.setOnClickListener(view2 -> {
+            fireIntent();
+        });
+
+        binding.toolbarComment.setOnClickListener(view3 -> {
+
+            saveComment();
+            fireIntent();
+        });
+
+        initValues();
+
+
+
+    }
+
+    private void initValues() {
+
+        String ImageUrl = "";
+
+        if(SubjectPost.getUser().getBoolean("HasUploadedPic")){
+            ImageUrl = SubjectPost.getUser().getParseFile("ProfilePic").getUrl();
+        }else{
+            ImageUrl = SubjectPost.getUser().getString("githubProfilePic");
+        }
+
+        if (!ImageUrl.isEmpty()) {
+            Glide.with(CommentActivity.this)
+                    .load(ImageUrl)
+                    .into(binding.ivProfileImage);
+        }
+    }
+
+    private void fireIntent() {
+
+        Intent intent = new Intent(CommentActivity.this, DetailsActivity.class);
+        intent.putExtra("post", SubjectPost);
+        startActivity(intent);
+        finish();
+    }
+
+    private void saveComment() {
 
     }
 }
