ffe0050e80ae121fe74ffa220a734057d7833bcc
Linus Brian Okoth <okoth@fb.com>
2020-07-23 09:11:50 -0400

    Innitialized the binding

--- app/src/main/java/com/example/devhub/Activities/EditProfile.java
+++ app/src/main/java/com/example/devhub/Activities/EditProfile.java
@@ -3,14 +3,27 @@ package com.example.devhub.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.devhub.R;
+import com.example.devhub.databinding.ActivityEditProfileBinding;
+import com.example.devhub.databinding.ActivityProfileBinding;
 
 public class EditProfile extends AppCompatActivity {
+
+    ActivityEditProfileBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        binding = ActivityEditProfileBinding.inflate(getLayoutInflater());
+        final View view = binding.getRoot();
+        setContentView(view);
+
+
+
+
+
     }
 }
