c4688a1e86d3889cd028c01a937713dd6ca6558f
Linus Brian Okoth <okoth@fb.com>
2020-07-24 11:30:56 -0400

    Added the setter and get methods of the likes

--- app/src/main/java/com/example/devhub/Models/Post.java
+++ app/src/main/java/com/example/devhub/Models/Post.java
@@ -55,7 +55,8 @@ public class Post extends ParseObject {
         put (KEY_TOPIC, topic);
     }
 
-    public List<User>
+    public List<ParseUser>getLikes(){return (List<ParseUser>)getJSONArray(KEY_LIKES);}
+    public void setLike( List<ParseUser> likes){ put(KEY_LIKES, likes);}
 
 
     public String getTime(){
