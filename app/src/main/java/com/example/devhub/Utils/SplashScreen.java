f09a236c529509b20dde4acf5800323d8d549531
Linus Brian Okoth <okoth@fb.com>
2020-07-25 12:30:30 -0400

    Overrided the oncreate activityb method

--- app/src/main/java/com/example/devhub/Utils/SplashScreen.java
+++ app/src/main/java/com/example/devhub/Utils/SplashScreen.java
@@ -1,6 +1,16 @@
 package com.example.devhub.Utils;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
 }
