f790efc115b5839e588f86b00ecc7cc1b3e37690
Linus Brian Okoth <okoth@fb.com>
2020-07-25 13:37:14 -0400

    Added date to be filled in at signUp

--- app/src/main/java/com/example/devhub/Utils/SplashScreen.java
+++ app/src/main/java/com/example/devhub/Utils/SplashScreen.java
@@ -1,9 +1,12 @@
 package com.example.devhub.Utils;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.devhub.Activities.MainActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -11,6 +14,8 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        startActivity(new Intent(this, MainActivity.class));
 
+        finish();
     }
 }
