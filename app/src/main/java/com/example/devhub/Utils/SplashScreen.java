bd981828fecc9c039e087d41fcc93657e0b34396
Linus Brian Okoth <okoth@fb.com>
2020-07-25 15:33:58 -0400

    Made a mofication in splash screen to save it

--- app/src/main/java/com/example/devhub/Utils/SplashScreen.java
+++ app/src/main/java/com/example/devhub/Utils/SplashScreen.java
@@ -6,15 +6,16 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.devhub.Activities.LoginActivity;
 import com.example.devhub.Activities.MainActivity;
+import com.parse.ParseUser;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        startActivity(new Intent(this, MainActivity.class));
+        startActivity(new Intent(this, LoginActivity.class));
 
         finish();
     }
