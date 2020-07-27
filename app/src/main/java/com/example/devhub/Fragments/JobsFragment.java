1bbf0d2e1b639f161895dd166a26ce6092459af2
Linus Brian Okoth <okoth@fb.com>
2020-07-27 14:02:59 -0400

    optimized imports of my view

--- app/src/main/java/com/example/devhub/Fragments/JobsFragment.java
+++ app/src/main/java/com/example/devhub/Fragments/JobsFragment.java
@@ -1,40 +1,15 @@
 package com.example.devhub.Fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.app.ActivityOptionsCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
-import android.transition.Transition;
-import android.transition.TransitionInflater;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.example.devhub.Activities.CommentActivity;
-import com.example.devhub.Activities.ComposeActivity;
-import com.example.devhub.Activities.DetailsActivity;
-import com.example.devhub.Activities.ProfileActivity;
-import com.example.devhub.Adapters.TimelineAdapter;
-import com.example.devhub.Models.Post;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import com.example.devhub.R;
-import com.example.devhub.Utils.EndlessRecyclerViewScrollListener;
-import com.parse.FindCallback;
-import com.parse.ParseUser;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class JobsFragment extends Fragment {
