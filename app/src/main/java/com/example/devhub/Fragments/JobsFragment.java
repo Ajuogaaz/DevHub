680ff201dc3bc39f666aa0edb59a9aea6676436b
Linus Brian Okoth <okoth@fb.com>
2020-07-27 14:12:15 -0400

    created the variable in the job fragments

--- app/src/main/java/com/example/devhub/Fragments/JobsFragment.java
+++ app/src/main/java/com/example/devhub/Fragments/JobsFragment.java
@@ -4,15 +4,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.devhub.Adapters.JobsAdapter;
+import com.example.devhub.Models.jobs;
 import com.example.devhub.R;
+import com.example.devhub.network.ApiClient;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class JobsFragment extends Fragment {
+
+    private RecyclerView mJobRecycler;
+    private JobsAdapter mJobsAdapter;
+    private ProgressBar loader;
+    private List<jobs> mJobList = new ArrayList<>();
+    private EditText edLocation, edDescription;
+    private Button btnSearch;
+    private ApiClient apiClient;
+    private boolean initialLoad = true;
 
 
 
