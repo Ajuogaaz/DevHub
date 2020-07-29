060816dd64ecf07ccdcb10ec4f7d071394cca712
Linus Brian Okoth <okoth@fb.com>
2020-07-29 00:28:44 -0400

    Added the Programming Language HashMap

--- app/src/main/java/com/example/devhub/AlgoModels/UserRep.java
+++ app/src/main/java/com/example/devhub/AlgoModels/UserRep.java
@@ -1,11 +1,11 @@
 package com.example.devhub.AlgoModels;
 
+import java.util.HashMap;
+
 public class UserRep {
 
-    public int numberOfRepo;
-    public int experience;
-    public String Language;
-    public String city;
+    HashMap<String, Integer> ProgrammingLanguage;
+
 
 
 
