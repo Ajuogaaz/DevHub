6e1b4f5735e10893ce0d03f7dc1e21fdba12fdae
Linus Brian Okoth <okoth@fb.com>
2020-07-29 12:26:22 -0400

    Creatyed the other profile activity_other_profile.xml

---
+++ app/src/main/java/com/example/devhub/Activities/OtherProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.devhub.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OtherProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other_profile);
+    }
+}
