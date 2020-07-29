f6c26b4bc5e9530564eaf6fb800a520930b1d151
Linus Brian Okoth <okoth@fb.com>
2020-07-29 00:34:59 -0400

    Created constructor and innitializing the repositories

--- app/src/main/java/com/example/devhub/AlgoModels/UserRep.java
+++ app/src/main/java/com/example/devhub/AlgoModels/UserRep.java
@@ -1,12 +1,28 @@
 package com.example.devhub.AlgoModels;
 
+import com.example.devhub.Models.Repositories;
+
 import java.util.HashMap;
+import java.util.List;
 
 public class UserRep {
 
     HashMap<String, Integer> ProgrammingLanguage;
 
+    List<String> Experience;
 
+    public UserRep(HashMap<String, Integer> programmingLanguage, List<String> experience) {
+        ProgrammingLanguage = programmingLanguage;
+        Experience = experience;
+    }
 
+    public static String innitializeDominantLanguage(List<Repositories> repos){
+
+        for(Repositories repo : repos){
+            String language = repo.getLanguage();
+
+        }
+
+    }
 
 }
