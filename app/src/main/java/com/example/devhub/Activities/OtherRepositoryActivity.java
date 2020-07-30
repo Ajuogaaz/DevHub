88bfd2ffce07a7c81629063542a695cf0b21075e
Linus Brian Okoth <okoth@fb.com>
2020-07-30 16:24:57 -0400

    Added the imports

--- app/src/main/java/com/example/devhub/Activities/OtherRepositoryActivity.java
+++ app/src/main/java/com/example/devhub/Activities/OtherRepositoryActivity.java
@@ -1,6 +1,8 @@
 package com.example.devhub.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -8,12 +10,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.devhub.Adapters.OtherRepositoryAdapter;
+import com.example.devhub.Adapters.RepositoryAdapter;
 import com.example.devhub.Models.Repositories;
 import com.example.devhub.R;
+import com.example.devhub.Utils.EndlessRecyclerViewScrollListener;
+import com.example.devhub.databinding.ActivityOtherProfileBinding;
 import com.example.devhub.network.ApiClient;
 import com.example.devhub.network.ApiService;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -22,10 +29,30 @@ import retrofit2.Response;
 
 public class OtherRepositoryActivity extends AppCompatActivity {
 
+    private RecyclerView rvRepos;
+    public static final String TAG = "OtherRepositoryActivity";
+    protected OtherRepositoryAdapter adapter;
+    protected List<Repositories> allRepos;
+    private SwipeRefreshLayout swipeContainer;
+    private EndlessRecyclerViewScrollListener scrollListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other_repository);
+
+
+        ParseUser.getCurrentUser().fetchInBackground();
+
+        CurrentUser = getIntent().getParcelableExtra("post");
+
+        posts = new ArrayList<>();
+
+        binding = ActivityOtherProfileBinding.inflate(getLayoutInflater());
+
+        final View view = binding.getRoot();
+        setContentView(view);
+
     }
 
     private void openWebView(Repositories repo) {
