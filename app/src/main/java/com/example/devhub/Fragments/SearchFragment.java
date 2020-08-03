4c078d52653660a826a92658fe02bd584915737d
Linus Brian Okoth <okoth@fb.com>
2020-08-03 16:45:35 -0400

    Imported the linear latyout abd binded the recycle view

--- app/src/main/java/com/example/devhub/Fragments/SearchFragment.java
+++ app/src/main/java/com/example/devhub/Fragments/SearchFragment.java
@@ -8,6 +8,7 @@ import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.devhub.Adapters.SearchAdapter;
