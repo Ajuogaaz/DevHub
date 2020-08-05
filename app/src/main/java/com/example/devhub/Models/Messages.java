3ab4d3111fa6b6a35463c31cab7041ef148410af
Linus Brian Okoth <okoth@fb.com>
2020-08-05 13:15:50 -0400

    Added the Messages

--- app/src/main/java/com/example/devhub/Models/Messages.java
+++ app/src/main/java/com/example/devhub/Models/Messages.java
@@ -1,4 +1,10 @@
 package com.example.devhub.Models;
 
 public class Messages {
+
+    public static final String KEY_BODY = "Body";
+    public static final String KEY_RECEIVING_USER = "ReceivingUser";
+    public static final String KEY_CREATED_AT = "createdAt";
+    public static final String KEY_SENDING_USER = "SendingUser";
+
 }
