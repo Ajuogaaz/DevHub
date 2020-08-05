1f4cfeaf43d0614bfcba1ad46a6394b054b705b1
Linus Brian Okoth <okoth@fb.com>
2020-08-05 12:31:31 -0400

    Added the Activity Chat binding

--- app/src/main/java/com/example/devhub/Activities/ChatActivity.java
+++ app/src/main/java/com/example/devhub/Activities/ChatActivity.java
@@ -3,14 +3,26 @@ package com.example.devhub.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.devhub.R;
+import com.example.devhub.databinding.ActivityChatBinding;
+import com.example.devhub.databinding.ActivityComposeBinding;
 
 public class ChatActivity extends AppCompatActivity {
+
+    ActivityChatBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
+
+        binding = ActivityChatBinding.inflate(getLayoutInflater());
+
+        final View view = binding.getRoot();
+        setContentView(view);
+
+
+
     }
 }
