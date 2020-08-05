5b7e48be94574e6277590f3e8ac8f632699ab4c9
Linus Brian Okoth <okoth@fb.com>
2020-08-04 21:51:43 -0400

    Added view binding to the FollowersActivity

--- app/src/main/java/com/example/devhub/Activities/FollowersActivity.java
+++ app/src/main/java/com/example/devhub/Activities/FollowersActivity.java
@@ -3,14 +3,24 @@ package com.example.devhub.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.devhub.R;
+import com.example.devhub.databinding.ActivityDetailsBinding;
+import com.example.devhub.databinding.ActivityFollowersBinding;
 
 public class FollowersActivity extends AppCompatActivity {
+
+    ActivityFollowersBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_followers);
+
+        binding = ActivityFollowersBinding.inflate(getLayoutInflater());
+
+        final View view = binding.getRoot();
+        setContentView(view);
+
     }
 }
