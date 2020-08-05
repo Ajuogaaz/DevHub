1b6eb6eb11cae0fc9ec4e5477054d1fcac579edc
Linus Brian Okoth <okoth@fb.com>
2020-08-04 21:24:06 -0400

    Added the empty activity_followers.xml

--- app/src/main/java/com/example/devhub/Activities/FollowersActivity.java
+++ app/src/main/java/com/example/devhub/Activities/FollowersActivity.java
@@ -1,4 +1,14 @@
 package com.example.devhub.Activities;
 
-public class FollowersActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FollowersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_followers);
+    }
 }
