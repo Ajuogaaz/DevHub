ee6d55fa316500847e4e57f13cf5071f69a96b4d
Linus Brian Okoth <okoth@fb.com>
2020-08-06 06:03:12 -0400

    Finished Innitializing view

--- app/src/main/java/com/example/devhub/Activities/ChatDetailsActivity.java
+++ app/src/main/java/com/example/devhub/Activities/ChatDetailsActivity.java
@@ -3,12 +3,23 @@ package com.example.devhub.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.devhub.R;
+import com.example.devhub.databinding.ActivityChatDetailsBinding;
+
+import java.util.ArrayList;
 
 public class ChatDetailsActivity extends AppCompatActivity {
+
+    ActivityChatDetailsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat_details);
+        binding = ActivityChatDetailsBinding.inflate(getLayoutInflater());
+        final View view = binding.getRoot();
+        setContentView(view);
+
     }
 }
