934f4a4effb5ef03adeab36fbb3a7fb91dfde065
Linus Brian Okoth <okoth@fb.com>
2020-08-06 00:26:24 -0400

    Added the MessageTop connection  to Parse

--- app/src/main/java/com/example/devhub/Models/MessageTop.java
+++ app/src/main/java/com/example/devhub/Models/MessageTop.java
@@ -1,8 +1,13 @@
 package com.example.devhub.Models;
 
+import com.parse.ParseClassName;
 import com.parse.ParseObject;
 
 import java.io.Serializable;
 
+
+@ParseClassName("MessageTop")
 public class MessageTop extends ParseObject implements Serializable {
+
+
 }
