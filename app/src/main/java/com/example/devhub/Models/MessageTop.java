7eb5701c99b442350509bcbed872cfbbfc2b42ef
Linus Brian Okoth <okoth@fb.com>
2020-08-06 00:27:27 -0400

    Added the variables from the Parse

--- app/src/main/java/com/example/devhub/Models/MessageTop.java
+++ app/src/main/java/com/example/devhub/Models/MessageTop.java
@@ -9,5 +9,8 @@ import java.io.Serializable;
 @ParseClassName("MessageTop")
 public class MessageTop extends ParseObject implements Serializable {
 
-
+    public static final String KEY_BODY = "TopMessage";
+    public static final String KEY_RECEIVING_USER = "UserOne";
+    public static final String KEY_CREATED_AT = "createdAt";
+    public static final String KEY_SENDING_USER = "UserTwo";
 }
