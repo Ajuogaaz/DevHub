98f5565567e21a2b8faf2b18e3a74eded3faca1e
Linus Brian Okoth <okoth@fb.com>
2020-08-07 20:44:42 -0400

    Added the page listeners

--- app/src/main/java/com/example/devhub/Activities/BoadingActivity.java
+++ app/src/main/java/com/example/devhub/Activities/BoadingActivity.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.devhub.Fragments.OnboardingFragment1;
 import com.example.devhub.Fragments.OnboardingFragment2;
@@ -44,8 +45,37 @@ public class BoadingActivity extends FragmentActivity {
             }
         };
 
+        binding.pager.setAdapter(adapter);
+        binding.indicator.setViewPager(binding.pager);
+
+        binding.skip.setOnClickListener(view2 -> finishOnboarding());
+        binding.next.setOnClickListener(view1 -> {
+            if(binding.pager.getCurrentItem() == 2) { // The last screen
+                finishOnboarding();
+            } else {
+                binding.pager.setCurrentItem(
+                        binding.pager.getCurrentItem() + 1,
+                        true
+                );
+            }
+        });
+
+        binding.indicator.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                if(position == 2){
+                    binding.skip.setVisibility(View.GONE);
+                    binding.next.setText("Done");
+                } else {
+                    binding.skip.setVisibility(View.VISIBLE);
+                    binding.next.setText("Next");
+                }
+            }
+        });
 
 
+    }
 
+    private void finishOnboarding() {
     }
 }
