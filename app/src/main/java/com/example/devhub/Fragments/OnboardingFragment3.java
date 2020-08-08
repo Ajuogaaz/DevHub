cdf2b5bd099ec663f3958c44e780706ff9c882ae
Linus Brian Okoth <okoth@fb.com>
2020-08-07 20:30:20 -0400

    Added both fragment 2 and 3

--- app/src/main/java/com/example/devhub/Fragments/OnboardingFragment3.java
+++ app/src/main/java/com/example/devhub/Fragments/OnboardingFragment3.java
@@ -1,4 +1,34 @@
 package com.example.devhub.Fragments;
 
-public class OnboardingFragment3 {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.devhub.R;
+
+import org.jetbrains.annotations.Nullable;
+
+public class OnboardingFragment3 extends Fragment{
+
+    public static final String TAG = OnboardingFragment3.class.getSimpleName();
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.onboarding_screen1, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
 }
