5aa176f58e488ced6cebdac46e17a73c2cb47b91
Linus Brian Okoth <okoth@fb.com>
2020-08-07 20:29:28 -0400

    Optimized imports in OnboardingFragment1

--- app/src/main/java/com/example/devhub/Fragments/OnboardingFragment1.java
+++ app/src/main/java/com/example/devhub/Fragments/OnboardingFragment1.java
@@ -1,4 +1,34 @@
 package com.example.devhub.Fragments;
 
-public class OnboardingFragment1 {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.devhub.R;
+
+import org.jetbrains.annotations.Nullable;
+
+public class OnboardingFragment1 extends Fragment{
+
+        public static final String TAG = OnboardingFragment1.class.getSimpleName();
+
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            View view = inflater.inflate(R.layout.onboarding_screen1, container, false);
+            return view;
+        }
+
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+
+
+        }
+
 }
