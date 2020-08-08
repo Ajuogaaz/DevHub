4a0aa6c7e6d7163e5a74d42b429a4dd9104f5095
Linus Brian Okoth <okoth@fb.com>
2020-08-07 20:33:00 -0400

    Changed onBoarding to inflate the Boarding Fragment

--- app/src/main/java/com/example/devhub/Activities/BoadingActivity.java
+++ app/src/main/java/com/example/devhub/Activities/BoadingActivity.java
@@ -1,6 +1,7 @@
 package com.example.devhub.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +9,7 @@ import android.view.View;
 import com.example.devhub.R;
 import com.example.devhub.databinding.ActivityBoadingBinding;
 
-public class BoadingActivity extends AppCompatActivity {
+public class BoadingActivity extends FragmentActivity {
 
     ActivityBoadingBinding binding;
 
