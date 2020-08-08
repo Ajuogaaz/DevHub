a8fa4e625c99d1577aa3fdf35e492dfb5b5095ea
Linus Brian Okoth <okoth@fb.com>
2020-08-08 01:19:48 -0400

    Added the saving button

--- app/src/main/java/com/example/devhub/Activities/BoadingActivity.java
+++ app/src/main/java/com/example/devhub/Activities/BoadingActivity.java
@@ -1,9 +1,6 @@
 package com.example.devhub.Activities;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,7 +12,6 @@ import com.example.devhub.Fragments.OnboardingFragment1;
 import com.example.devhub.Fragments.OnboardingFragment2;
 import com.example.devhub.Fragments.OnboardingFragment3;
 import com.example.devhub.R;
-import com.example.devhub.databinding.ActivityBoadingBinding;
 import com.ogaclejapan.smarttablayout.SmartTabLayout;
 
 public class BoadingActivity extends FragmentActivity {
@@ -25,8 +21,7 @@ public class BoadingActivity extends FragmentActivity {
 
     private ViewPager pager;
     private SmartTabLayout indicator;
-    private Button skip;
-    private Button next;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +30,6 @@ public class BoadingActivity extends FragmentActivity {
 
         pager = findViewById(R.id.pager);
         indicator = findViewById(R.id.indicator);
-        skip = findViewById(R.id.skip);
-        next = findViewById(R.id.next);
 
         adapter = new FragmentStatePagerAdapter(getSupportFragmentManager()) {
             @NonNull
@@ -59,41 +52,9 @@ public class BoadingActivity extends FragmentActivity {
         pager.setAdapter(adapter);
         indicator.setViewPager(pager);
 
-        skip.setOnClickListener(view2 -> finishOnboarding());
-        next.setOnClickListener(view1 -> {
-            if(pager.getCurrentItem() == 2) { // The last screen
-                finishOnboarding();
-            } else {
-                pager.setCurrentItem(
-                        pager.getCurrentItem() + 1,
-                        true
-                );
-            }
-        });
 
-        indicator.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                if(position == 2){
-                    skip.setVisibility(View.GONE);
-                    next.setText("Done");
-                } else {
-                    skip.setVisibility(View.VISIBLE);
-                    next.setText("Next");
-                }
-            }
-        });
 
 
     }
 
-    private void finishOnboarding() {
-
-        // Launch the main Activity, called MainActivity
-        Intent main = new Intent(BoadingActivity.this, ValidateActivity.class);
-        startActivity(main);
-
-        // Close the OnboardingActivity
-        finish();
-    }
 }
