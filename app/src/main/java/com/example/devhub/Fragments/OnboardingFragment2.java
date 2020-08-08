c7f35c35ab2b262aa3edd77acbb776a36dd620e0
Linus Brian Okoth <okoth@fb.com>
2020-08-07 20:30:46 -0400

    Inflated the right layout

--- app/src/main/java/com/example/devhub/Fragments/OnboardingFragment2.java
+++ app/src/main/java/com/example/devhub/Fragments/OnboardingFragment2.java
@@ -20,7 +20,7 @@ public class OnboardingFragment2 extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.onboarding_screen1, container, false);
+        View view = inflater.inflate(R.layout.onboarding_screen2, container, false);
         return view;
     }
 
--- app/src/main/java/com/example/devhub/Fragments/OnboardingFragment3.java
+++ app/src/main/java/com/example/devhub/Fragments/OnboardingFragment3.java
@@ -20,7 +20,7 @@ public class OnboardingFragment3 extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.onboarding_screen1, container, false);
+        View view = inflater.inflate(R.layout.onboarding_screen3, container, false);
         return view;
     }
 
